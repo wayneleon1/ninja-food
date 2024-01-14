db8c18090bb08bb9125a84be6f7e2fdee86c5a95
wayneleon1 <rurangwaleon10@gmail.com>
2024-01-14 10:34:34 +0200

    tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
   },
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
+  },
+  variants: {},
   plugins: [],
 };
